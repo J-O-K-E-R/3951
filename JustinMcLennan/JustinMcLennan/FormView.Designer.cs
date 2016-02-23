9cef5b2f05138c9b7f63f4b059cc9d22fdf568ee
Justin <tribekiller911@gmail.com>
2016-02-23 11:27:30 -0800

    midterm

---
+++ JustinMcLennan/JustinMcLennan/FormView.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace JustinMcLennan
+{
+    partial class FormView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerMoving = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timerMoving
+            // 
+            this.timerMoving.Enabled = true;
+            this.timerMoving.Interval = 10;
+            this.timerMoving.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(306, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // FormView
+            // 
+            this.DoubleBuffered = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 228);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormView";
+            this.Text = "Midterm Comp 3951 2016 Justin McLennan";
+            this.Resize += new System.EventHandler(this.FromView_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Timer timerMoving;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ JustinMcLennan/JustinMcLennan/FormView.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace JustinMcLennan
+{
+    /// <summary>
+    /// This will draw a moving square
+    /// Author: Justin McLennan
+    /// </summary>
+    public partial class FormView : Form
+    {
+        /// <summary>
+        /// Different directions that the box can move are marked here
+        /// </summary>
+        enum Position
+        {
+            DLeft, DRight, ULeft, URight
+        }
+        //all used variables are put here since they are used 
+        //throughout multiple methods
+        private int _x;
+        private int _y;
+        private int _width;
+        private int _height;
+        private Color color1 = rancolor();
+        private Color color2 = rancolor();
+        private Position _objPosition;
+
+        public FormView()
+        {
+            InitializeComponent();
+            _x = 50;
+            _y = 50;
+            _width = this.ClientRectangle.Width/10;
+            _height = this.ClientRectangle.Width/10;
+            _objPosition = Position.DLeft;
+        }
+        /// <summary>
+        /// Used to generate 2 random colors for inside square
+        /// </summary>
+        /// <returns></returns>
+        private static Color rancolor()
+        {
+            Random randomGen = new Random();
+            KnownColor[] names = (KnownColor[])Enum.GetValues(typeof(KnownColor));
+            KnownColor randomColorName = names[randomGen.Next(names.Length)];
+            Color randomColor = Color.FromKnownColor(randomColorName);
+            return randomColor;
+        }
+        /// <summary>
+        /// Draws the box and creates the brush for it
+        /// This is not a class noticed the requirement too late to implement properly
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Square(object sender, PaintEventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            //create a square for the path
+            Rectangle myRec = new Rectangle(_x, _y, _width, _height);
+            path.AddRectangle(myRec);
+            //create a gradient brush
+            PathGradientBrush pthGrBrush = new PathGradientBrush(path);
+            Color[] color = { Color.Orange, color1, color2 };
+            float[] relativePositions = {
+                0f,       
+                0.4f,     
+                1.0f};
+            //creates a color blend with orange and 2 random colors
+            ColorBlend colorBlend = new ColorBlend();
+            colorBlend.Colors = color;
+            //marks positions of the colors
+            colorBlend.Positions = relativePositions;
+            //adds the colors to the path
+            pthGrBrush.InterpolationColors = colorBlend;
+            //paints the square
+            e.Graphics.FillRectangle(pthGrBrush, _x, _y, _width, _height);
+        }
+        /// <summary>
+        /// Called when the window is reisized
+        /// will resize the moving square based on window size
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FromView_Resize(object sender, EventArgs e)
+        {
+            //Used the same side so it is kept a square
+            //instead of a rectangle
+            _width = this.ClientRectangle.Width / 10;
+            _height = this.ClientRectangle.Width / 10;
+        }
+        /// <summary>
+        /// Window timer allows for set window timer intervals
+        /// to refresh the form and invalidate the new position
+        /// of the rectangle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //sends bounds for Down Right Movement
+            if(_objPosition == Position.DRight){
+                _x += 1;
+                _y += 1;
+                if(_x+_width > this.ClientRectangle.Width){
+                    _objPosition = Position.DLeft;
+                }
+                if(_y+_height > this.ClientRectangle.Height){
+                    _objPosition = Position.URight;
+                }
+                
+            }
+            //sets bounds for down left movement
+            else if(_objPosition == Position.DLeft){
+                _x -= 1;
+                _y += 1;
+                if (_x < 0)
+                {
+                    _objPosition = Position.DRight;
+                }
+                if (_y +_height > this.ClientRectangle.Height)
+                {
+                    _objPosition = Position.ULeft;
+                }
+            }
+            //sets bounds for up right movement
+            else if(_objPosition == Position.URight){
+                _x += 1;
+                _y -= 1;
+                if (_x + _width > this.ClientRectangle.Width)
+                {
+                    _objPosition = Position.ULeft;
+                }
+                if (_y < 25)
+                {
+                    _objPosition = Position.DRight;
+                }
+            }
+            //sets bounds for up left movement
+            else if (_objPosition == Position.ULeft)
+            {
+                _x -= 1;
+                _y -= 1;
+                if (_x < 0)
+                {
+                    _objPosition = Position.URight;
+                }
+                if (_y < 25)
+                {
+                    _objPosition = Position.DLeft;
+                }
+            }
+            Invalidate();
+        }
+        /// <summary>
+        /// Initially draws the box but then allows movement by stopping
+        /// and starting the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //marks the forms paint method on first use
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Square);
+            //starts the timer
+            this.timerMoving.Enabled = true;
+        }
+        /// <summary>
+        /// Stops the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //stops the timer
+            this.timerMoving.Enabled = false;
+        }
+        /// <summary>
+        /// Closes the window
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
