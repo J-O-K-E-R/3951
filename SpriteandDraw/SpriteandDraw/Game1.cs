efaef0264d99718cf04f64abdd0d66682f321c53
Justin <tribekiller911@gmail.com>
2016-03-10 16:58:37 -0800

    playing around

---
+++ SpriteandDraw/SpriteandDraw/Game1.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+using ProjectName;
+
+namespace SpriteandDraw {
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        List<Circle> circles = new List<Circle>();
+        private SpriteFont font;
+        //private double score = 0;
+
+        public Game1() {
+            graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1600;  // set this value to the desired width of your window
+            graphics.PreferredBackBufferHeight = 900;   // set this value to the desired height of your window
+            graphics.ApplyChanges();
+            Content.RootDirectory = "Content";
+        }
+        protected void CreateCircle() {
+            //width starts at -50 - 1550
+            int x = 735;
+            int[] y = {805, 770, 735, 700};
+            for(int i = 0; i < y.Length; i++)
+            {
+                Console.WriteLine(GraphicsDevice.Viewport.Width);
+                circles.Add(new Circle(new Vector2(x, y[i])));
+            }
+        }
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize() {
+            // TODO: Add your initialization logic here
+            this.IsMouseVisible = true;
+
+
+            CreateCircle();
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent() {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            font = Content.Load<SpriteFont>("alphabet");
+            Circle.LoadContent(Content);
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent() {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime) {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            foreach (Circle circle in circles)
+                circle.Draw(spriteBatch);
+            //spriteBatch.DrawString(font, "Chung what the hell are you doing: " + score, new Vector2(100, 100), Color.Black);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
